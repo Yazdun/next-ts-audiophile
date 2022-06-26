02879ec74efa470e663310cc0f4bd3f3f135bfe5
Yazdun <yazduntube@gmail.com>
2022-06-26 18:08:56 +0430

    general : made progress

--- components/cart/index.tsx
+++ components/cart/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
+import { AiOutlineShoppingCart } from 'react-icons/ai'
+import css from './styles.module.css'
 
 export const Cart: React.FC = () => {
-  return <div>Cart</div>
+  return (
+    <div>
+      <button className={css.cta}>
+        <AiOutlineShoppingCart />
+      </button>
+    </div>
+  )
 }
--- components/hamburger/index.tsx
+++ components/hamburger/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
+import { Squash as BurgerButton } from 'hamburger-react'
+import css from './styles.module.css'
 
 export const Hamburger: React.FC = () => {
-  return <div>Hamburger</div>
+  return (
+    <div>
+      <div className={css.cta}>
+        <BurgerButton size={25} />
+      </div>
+    </div>
+  )
 }
