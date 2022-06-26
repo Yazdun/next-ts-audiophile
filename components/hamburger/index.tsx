6c24fff6ecc307fb2f1e7f2a59eb03cf39bf5ebc
Yazdun <yazduntube@gmail.com>
2022-06-26 22:48:08 +0430

    general : made progress

--- components/hamburger/index.tsx
+++ components/hamburger/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Squash as BurgerButton } from 'hamburger-react'
 import css from './styles.module.css'
 import { useState } from 'react'
@@ -7,21 +7,33 @@ import { Category } from '@components/index'
 import { AnimatePresence, motion } from 'framer-motion'
 import { framer_menu_desktop, framer_menu_mobile } from './framer'
 import { useWindowSize, useIsMounted } from '@hooks/index'
+import { useOnClickOutside } from 'usehooks-ts'
 
 export const Hamburger: React.FC = () => {
   const [open, setOpen] = useState(false)
   const isMounted = useIsMounted()
   const { isTouch } = useWindowSize()
+  const ref = useRef(null)
   const animations = isTouch ? framer_menu_mobile : framer_menu_desktop
+
+  useOnClickOutside(ref, () => setOpen(false))
+
+  useEffect(() => {
+    setOpen(false)
+  }, [isTouch])
 
   if (!isMounted) {
     return null
   }
 
   return (
-    <>
+    <div ref={ref}>
       <div className={css.cta}>
-        <BurgerButton size={25} onToggle={() => setOpen(prev => !prev)} />
+        <BurgerButton
+          size={25}
+          toggled={open}
+          onToggle={() => setOpen(prev => !prev)}
+        />
       </div>
       <AnimatePresence>
         {open && (
@@ -34,6 +46,6 @@ export const Hamburger: React.FC = () => {
           </motion.div>
         )}
       </AnimatePresence>
-    </>
+    </div>
   )
 }
