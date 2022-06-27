62125d2e02869bf204ba45e62c5904b9b6c26193
Yazdun <yazduntube@gmail.com>
2022-06-27 22:45:55 +0430

    general : made progress

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -18,6 +18,7 @@ interface IProps {
   transparent?: boolean
   children: React.ReactNode
   href?: string
+  maxwidth?: number
 }
 
 export const Button: React.FC<IProps> = ({
@@ -26,18 +27,23 @@ export const Button: React.FC<IProps> = ({
   transparent,
   children,
   href,
+  maxwidth,
 }) => {
   const variants = cn(
     css.btn,
     primary && css.primary,
     outline && css.outline,
     transparent && css.transparent,
+    href && css.href,
   )
 
   if (href) {
     return (
       <Link href={href}>
-        <a className={variants}>
+        <a
+          className={variants}
+          style={{ maxWidth: maxwidth ? maxwidth : '100%' }}
+        >
           {children}
           <MdNavigateNext className={css.icon} />
         </a>
--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -7,13 +7,35 @@
 *-------------------------------------------------------------------*/
 
 import React from 'react'
+import { Button } from '@components/index'
 import css from './styles.module.css'
+import { home_desktop_images } from '@assets/index'
+import Image from 'next/image'
 
 export const Hero: React.FC = () => {
   return (
     <div className={css.wrapper}>
-      <div className={css.cover}></div>
-      <div className={css.container}>Hero</div>
+      <div className={css.slice}></div>
+      <div className={css.container}>
+        <div className={css.text}>
+          <p className={css.adv}>NEW PRODUCT</p>
+          <h1 className={css.title}>
+            XX99 Mark II
+            <span>HeadphoneS</span>
+          </h1>
+
+          <p className={css.desc}>
+            Experience natural, lifelike audio and exceptional build quality
+            made for the passionate music enthusiast.
+          </p>
+          <Button href="/product" maxwidth={120}>
+            see product
+          </Button>
+        </div>
+        <div className={css.img}>
+          <Image src={home_desktop_images.image_headset} alt="headset" />
+        </div>
+      </div>
     </div>
   )
 }
