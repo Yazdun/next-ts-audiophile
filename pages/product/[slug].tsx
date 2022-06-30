b1d8ecb701618a4061ec9af5e3f9caf8c3fb3e39
Yazdun <yazduntube@gmail.com>
2022-06-30 17:56:43 +0430

    general : made progress

--- pages/product/[slug].tsx
+++ pages/product/[slug].tsx
@@ -1,22 +1,51 @@
 import type { NextPage } from 'next'
+import { GetStaticProps, GetStaticPaths } from 'next'
 import { Layout, SEO } from '@components/index'
 import { IProduct } from '@models/product'
 import css from './styles.module.css'
+import { products } from '@data/index'
+import { ParsedUrlQuery } from 'querystring'
 
 interface IProps {
-  products: IProduct[]
-  title: string
+  product: IProduct
 }
 
 const ProductPage: NextPage<IProps> = props => {
+  const { product } = props
   return (
     <>
       <SEO title="Product" />
       <Layout>
-        <h1>Product</h1>
+        <div className={css.container}>
+          <h1>{product.name}</h1>
+        </div>
       </Layout>
     </>
   )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = products.map(product => {
+    return {
+      params: { slug: product.slug || '' },
+    }
+  })
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+interface IParams extends ParsedUrlQuery {
+  slug: string
+}
+
+export const getStaticProps: GetStaticProps = async context => {
+  const { slug } = context.params as IParams
+  const data = products.find(product => product.slug === slug)
+  return {
+    props: { product: data },
+  }
 }
 
 export default ProductPage
