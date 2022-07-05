3e6939918e57412d62a8becdf58bc3390a432fdd
Yazdun <yazduntube@gmail.com>
2022-07-05 14:30:47 +0430

    general : made progress

--- components/quantity/framer.ts
+++ components/quantity/framer.ts
@@ -1,6 +1,6 @@
 export const framer_count = {
-  initial: { opacity: 0, y: '-100%' },
+  initial: { opacity: 0, y: '-80%' },
   animate: { opacity: 1, y: 0 },
-  exit: { opacity: 0, y: '100%' },
+  exit: { opacity: 0, y: '80%' },
   transition: { duration: 0.3 },
 }
